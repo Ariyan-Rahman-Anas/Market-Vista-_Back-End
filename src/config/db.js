e8543a806aecfaa1e6ef45ffb5920f114912573a
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-01-17 04:01:58 +0600

    Yahooo! Now the server is successfully connected with mongoDB

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const { DB_URI } = require("../secret")
+
+const connectDataBase = async (options={}) => {
+    try {
+        await mongoose.connect(DB_URI, options);
+        console.log("Successfully connected with MongoDB!")
+
+        mongoose.connection.on("error", (error) => {
+            console.error("DB connection error: ", error)
+        })
+    } catch (error) {
+      console.error("Could not connect to DB: ", error.toString());
+    }
+}
+
+module.exports = connectDataBase
--- src/secret.js
+++ src/secret.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
 const serverPort = process.env.PORT || 5002;
+const DB_URI = process.env.DB_URI || "mongodb://localhost:27017/MarketVista";
 
-module.exports = {serverPort}
+module.exports = {serverPort, DB_URI}
