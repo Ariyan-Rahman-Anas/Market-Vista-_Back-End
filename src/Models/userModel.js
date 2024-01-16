984b91613f2f2b8e75d01b1b1851c2662e002f9d
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-01-17 05:07:47 +0600

    Designed user schema and model

--- src/Models/userModel.js
+++ src/Models/userModel.js
@@ -1,8 +1,59 @@
-const users = [
-  { id: 1, name: "Araber Rahman" },
-  { id: 2, name: "Amena Akter Shilpi" },
-  { id: 3, name: "Ariyan Rahman Anas" },
-  { id: 4, name: "Maniruzzaman Abbas" },
-];
+const { Schema, model } = require("mongoose")
+const bcrypt = require("bcrypt");
+const { defaultImgPath } = require("../secret");
 
-module.exports = {users}
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "User name is required"],
+    trim: true,
+    minlength: [3, "The length of user name can be minimum of 3 character"],
+    maxlength: [31, "The length of user name can be maximum of 31 character"],
+  },
+  email: {
+    type: String,
+    required: [true, "User email is required"],
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function (v) {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+      },
+      message: "Please enter a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, " User password is required"],
+    minlength: [6, "The length of user password can be minimum of 6 character"],
+    set: (v) => bcrypt.hashSync(v, bcrypt.genSaltSync(10)),
+  },
+  image: {
+    type: String,
+    default: defaultImgPath
+  },
+  address: {
+    type: String,
+    required: [true, "User address is required"],
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: [true, "User phone number is required"],
+    trim: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isBanned: {
+    type: Boolean,
+    default: false,
+  },
+}, { timestamps: true });
+
+const User = model("Users,", userSchema)
+
+module.exports = User
--- src/secret.js
+++ src/secret.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
+
 const serverPort = process.env.PORT || 5002;
 const DB_URI = process.env.DB_URI || "mongodb://localhost:27017/MarketVista";
 
-module.exports = {serverPort, DB_URI}
+const defaultImgPath = process.env.DEFAULT_USER_IMG || "/public/images/users/avatar.png"
+
+module.exports = {serverPort, DB_URI, defaultImgPath}
