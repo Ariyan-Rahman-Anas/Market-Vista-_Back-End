68c9b6b2c8f5a3f4fc575288b7a2ccaa68577f0a
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-01-17 00:48:56 +0600

    trying to set up the middleware and secured the API

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const createError = require("http-errors")
+const xssClean = require("xss-clean")
+const rateLimit = require("express-rate-limit")
+const app = express();
+
+//creating limit
+const rateLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 mnt
+    max: 5,
+    message: "Too many request from this IP. Please try again later.",
+})
+
+//middleware
+app.use(rateLimiter)
+app.use(xssClean());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "Welcome to the MarketVista",
+  });
+});
+app.get("/api/user", (req, res) => {
+  console.log(req.body.id);
+  res.status(200).send({
+    message: "User  is returned!",
+  });
+});
+
+//client error handling
+app.use((req, res, next) => {
+  next(createError(404, "Route not found"));
+});
+
+//server error handling
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        success: false,
+        message: err.message
+    })
+});
+
+module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
-const express = require("express")
-const port = 5000
-const app = express()
-
-app.get("/test", (req, res) => {
-    res.status(200).send({
-        message: "Welcome to the MarketVista"
-    })
-})
+const app = require("./app")
+const port = 5001;
 
 app.listen(port, () => {
-    console.log(`The MarketVista Server is running on:  ${port}`)
+    console.log(`The MarketVista Server is running http://localhost:${port}`)
 })
+
+
+
+
+//no need this //middleware
+// const isLoggedIn = (req, res, next) => {
+//   console.log("This is the isLoggedIn middleware");
+//   const logIn = true;
+//   if (logIn == true) {
+//     req.body.id = 101
+//     next();
+//   } else {
+//     res.status(401).json({ massage: "Please Log in first to access your profile." });
+//   }
+// }
